352f520dd3606cbf6a52cd8eba074bb178e6fb89
David <kpdmmvi@gmail.com>
2025-11-12 21:28:33 -0500

    Removed uneeded code

--- src/TEAM21Proj4B.c
+++ src/TEAM21Proj4B.c
@@ -1,38 +1,13 @@
 #include "hat.h"
 #include <stdio.h>
 
-#define ADC_CHANNEL 10 // PC0
 #define PWM_FREQ_HZ 50 // Standard servo PWM frequency
-#define TIM3_FREQ_HZ 1000000 // 1MHz timer clock for 1us resolution
-#define TIM3_ARR ((TIM3_FREQ_HZ / PWM_FREQ_HZ) - 1) // Auto-reload value for 50Hz (20000 - 1 = 19999)
-#define FEEDBACK_PERIOD_US 1100
-#define FEEDBACK_MAX_DUTY_CYCLE 0.971f // Corresponds to 1070us pulse width
-#define FEEDBACK_MIN_DUTY_CYCLE 0.029f // Corresponds to 30us pulse width
 #define SERVO_NEUTRAL_PULSE_WIDTH 1500 // 1.5ms pulse width for neutral position
 
 #define CW_MAX_PULSE 1480 
 #define CW_MIN_PULSE 1280
 #define CCW_MIN_PULSE 1520
 #define CCW_MAX_PULSE 1720
-
-SERVO_t left_wheel ={
-    .SERVO_PIN_PORT = GPIOC,
-    .SERVO_PWM_PIN = 6, //PC6
-    .SERVO_FEEDBACK_PIN =7 //not used
-};
-
-SERVO_t right_wheel ={
-    .SERVO_PIN_PORT = GPIOC,
-    .SERVO_PWM_PIN = 8, //PC8
-    .SERVO_FEEDBACK_PIN =9 //not used
-};
-
-volatile uint8_t direction = 0; // 0 for CW, 1 for CCW
-volatile uint8_t stop = 1;
-volatile uint8_t value_ready = 0; //Flag to indicate new ADC value is ready
-volatile uint8_t sensor_values[4] = {0, 0, 0, 0};//Array for sensor values
-volatile uint32_t servo1_pulse_width = SERVO_NEUTRAL_PULSE_WIDTH; 
-volatile uint32_t servo2_pulse_width = SERVO_NEUTRAL_PULSE_WIDTH;
 
 void SysTick_Handler(void){
 }
